9cba1dc3f3d0ee2e87d36e2680fcb86c24bd93b0
aglaeDev <a.gourdet.dev@gmail.com>
2023-07-18 20:12:59 +0200

    avant dernier commit

--- app_3.rb
+++ app_3.rb
@@ -5,3 +5,20 @@ require_relative 'lib/game'
 require_relative 'lib/player'
 
 my_game = Game.new("Wolverine")
+
+puts "------------------------------------------------"
+puts "|Bienvenue sur 'ILS VEULENT TOUS MA POO' !      |"
+puts "|Le but du jeu est d'être le dernier survivant ici !|"
+puts "-------------------------------------------------"
+
+puts "comment tu t'apelle ?"
+name_of_human_player = gets.chomp
+
+my_game.initialize
+#while user.life_points > 0 && (player1.life_points > 0 || player2.life_points >0)
+my_game.menu_choice
+puts "Les autres joueurs t'attaquent !"
+my_game.enemies_attack
+#end
+my_game.end
+
